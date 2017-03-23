a0290a231afd85f6941e7ea7ee142b5fb0198e63
Lorenzo Romagnoli <lorenzormgnl@gmail.com>
2017-03-23 15:52:25 +0800

    scorporated scripts from footer

--- site/themes/automato/static/js/script.js
+++ site/themes/automato/static/js/script.js
@@ -137,25 +137,6 @@ $(document).ready(function() {
 
     $(".flex-video").fitVids();
 
-    $('.masonryGrid').imagesLoaded(function() {
-
-        var $masonryGrid=$('.masonryGrid').masonry({
-            itemSelector: '.grid-item',
-            columnWidth: '.grid-sizer',
-            percentPosition: true
-        });
-    });
-
-    try {
-      $('#experiments iframe').load(function(){
-        var $masonryGrid=$('.masonryGrid').masonry({
-            itemSelector: '.grid-item'
-        });
-      });
-    }
-    catch(err) {
-        console.log("iframe not ready",err);
-    }
 
 
 
@@ -210,24 +191,7 @@ $(document).ready(function() {
 
     theFuture();
 
-    // Twitter widget is loading.
-      window.twttr = function (d, s, id) {
-      var t, js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) return; js = d.createElement(s); js.id = id;
-      js.src = "https://platform.twitter.com/widgets.js";
-      fjs.parentNode.insertBefore(js, fjs);
-      return window.twttr || (t = { _e: [], ready: function (f) { t._e.push(f) } });
-      }(document, 'script', 'twitter-wjs');
-
-    // When widget is ready, run masonry
-    twttr.ready(function (twttr) {
-      twttr.events.bind('loaded', function (event) {
-        var $masonryGrid=$('.masonryGrid').masonry({
-            itemSelector: '.grid-item'
-        });
-      });
-    });
-
+  
 });
 
 
